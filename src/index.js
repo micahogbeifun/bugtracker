5a13d7c1adfeaa06e82d5e7aa93334ec92b2ccc6
Ubuntu <ubuntu@ip-172-31-31-208.us-east-2.compute.internal>
2021-09-04 10:56:39 +0000

    change

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
 const app = (
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL} >
       <App />
     </BrowserRouter>
   </Provider>
